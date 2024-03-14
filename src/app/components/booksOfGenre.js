d83a9e789030134a7a387b68058ae06a1fbec11b
mecoupe <69225308+Mik-watanabe@users.noreply.github.com>
2024-03-14 12:12:47 -0400

    connect page to book page

--- src/app/components/booksOfGenre.js
+++ src/app/components/booksOfGenre.js
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Link from "next/link";
 
 //Receives a genre as a prop, calls the api with it, and shows the receieved books.
 export default function BooksOfGenre(props) {
@@ -25,18 +26,20 @@ export default function BooksOfGenre(props) {
         {bookData.items.map((book) => {
           console.log(book);
           return (
-            <div className=" w-[25%] carousel-item">
-              <figure>
-                <img
-                //Returns another image saying "Image not found" if the api does not return any image links. Size has been
-                  src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg" }
-                  className="w-[10vw]"
-                  alt={book.volumeInfo.title}
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title w-[20px]">{book.volumeInfo.title}</h2>
-              </div>
+            <div className=" w-[25%] carousel-item" key={book.id}>
+              <Link href={`/book/${book.id}`} className="w-full flex">
+                <figure>
+                  <img
+                  //Returns another image saying "Image not found" if the api does not return any image links. Size has been
+                    src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg" }
+                    className="w-[10vw]"
+                    alt={book.volumeInfo.title}
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title w-[20px]">{book.volumeInfo.title}</h2>
+                </div>
+              </Link>
             </div>
           );
         })}
