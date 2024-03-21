5a80477e4c3bd0367d06553c6fcd70343ede83a4
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-21 11:42:46 -0400

    Add validation in case no results are found

--- src/app/components/booksOfGenre.js
+++ src/app/components/booksOfGenre.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 require("dotenv").config();
 // remove this after
 import { useEffect, useState } from "react";
@@ -17,7 +17,7 @@ export default function BooksOfGenre(props) {
         data: { query: `subject:${props.genre}` },
       });
       setBookData(response.data);
-    }
+    };
     fetchData();
   }, []);
 
@@ -25,27 +25,34 @@ export default function BooksOfGenre(props) {
     <div>
       <h1 className="mb-4 text-4xl font-extrabold">{props.genre}</h1>
       <div className="carousel carousel-left p-4 space-x-4 bg-neutral">
-        {bookData.items.map((book) => {
-          return (
-            <div className=" w-[25%] carousel-item">
-              <figure>
-                <img
-                  //Returns another image saying "Image not found" if the api does not return any image links. Size has been
-                  src={
-                    book.volumeInfo.imageLinks
-                      ? book.volumeInfo.imageLinks.thumbnail
-                      : "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg"
-                  }
-                  className="w-[10vw]"
-                  alt={book.volumeInfo.title}
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title w-[20px]">{book.volumeInfo.title}</h2>
-              </div>
-            </div>
-          );
-        })}
+        {
+          //Returns "No results found if no matching results are found
+          bookData.items
+            ? bookData.items.map((book) => {
+                return (
+                  <div className=" w-[25%] carousel-item">
+                    <figure>
+                      <img
+                        //Returns another image saying "Image not found" if the api does not return any image links. Size has been
+                        src={
+                          book.volumeInfo.imageLinks
+                            ? book.volumeInfo.imageLinks.thumbnail
+                            : "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg"
+                        }
+                        className="w-[10vw]"
+                        alt={book.volumeInfo.title}
+                      />
+                    </figure>
+                    <div className="card-body">
+                      <h2 className="card-title w-[20px]">
+                        {book.volumeInfo.title}
+                      </h2>
+                    </div>
+                  </div>
+                );
+              })
+            : "No results found"
+        }
       </div>
     </div>
   ) : (
