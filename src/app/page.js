1b8d08d58342f1f2ca4591159deb214d87c7ee5b
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-28 12:24:33 -0400

    Use intro component on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,17 @@
-import Navbar from '@/app/components/navbar'
-import BooksOfGenre from '@/app/booksOfGenre'
+import Navbar from "@/app/components/navbar";
+import Intro from "@/app/components/intro";
+import BooksOfGenre from "@/app/booksOfGenre";
 
-export default function App(){
-  return (<div>
-    <Navbar></Navbar>
-    <BooksOfGenre genre="Fiction"/>
-    <br/>
-    <BooksOfGenre genre="Fantasy"/>
-  </div>)
+export default function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Intro>
+
+      </Intro>
+      <BooksOfGenre genre="Fiction" />
+      <br />
+      <BooksOfGenre genre="Fantasy" />
+    </div>
+  );
 }
