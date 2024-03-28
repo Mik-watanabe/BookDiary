83078c84253c4351926a48d0aa3e457e75cc69d2
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-28 12:20:01 -0400

    Change directory structure part 2

---
+++ src/app/components/intro/index.js
@@ -0,0 +1,11 @@
+import SearchBar from "@/app/components/searchBar";
+
+//Intro part of the page. Includes welcome and search bar
+export default function Intro({ children }) {
+  return (
+    <>
+      <div className="flex flex-col items-center">{children}</div>
+      <SearchBar/>
+    </>
+  );
+}
