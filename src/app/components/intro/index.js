b84d99959de9162160960becf8383e733371b1e6
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-28 21:34:18 -0400

    Make the intro component a flexbox

--- src/app/components/intro/index.js
+++ src/app/components/intro/index.js
@@ -1,11 +1,11 @@
 import SearchBar from "@/app/components/searchBar";
 
 //Intro part of the page. Includes welcome and search bar
-export default function Intro({ children }) {
+export default function Intro(props) {
   return (
     <>
-      <div className="flex flex-col items-center w-screen">
-        {children}
+      <div className={`flex flex-col items-center w-screen h-[450px] justify-between ${props.className}`}>
+        {props.children}
         <SearchBar />
       </div>
     </>
