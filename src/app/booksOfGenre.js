bc8e470552d35b6c176a5c022cc26b5cddfd747d
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-28 21:31:46 -0400

    Update genre book display
    Doesn't use daisyUI in the book display anymore. The images of different sizes look strange. The title text needs to be styled

--- src/app/booksOfGenre.js
+++ src/app/booksOfGenre.js
@@ -1,9 +1,9 @@
 "use client";
 require("dotenv").config();
-// remove this after
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Link from "next/link";
+import Image from "next/image";
 
 let APIKEY = process.env.APIKEY;
 //Receives a genre as a prop, calls the api with it, and shows the receieved books.
@@ -23,40 +23,45 @@ export default function BooksOfGenre(props) {
   }, []);
 
   return bookData ? (
-    <div>
-      <h1 className="mb-4 text-4xl font-extrabold">{props.genre}</h1>
-      <div className="carousel carousel-left p-4 space-x-4">
-        {
-          //Returns "No results found if no matching results are found
-          bookData.items
-            ? bookData.items.map((book) => {
-                console.log(book);
-                return (
-                  <div className=" w-[25%] carousel-item" key={book.id}>
-                    <Link href={`/book/${book.id}`} className="w-full flex">
-                      <figure>
-                        <img
+    <div className={props.className}>
+      <h1 className="mb-4 text-4xl text-center">{props.genre}</h1>
+      <div className="w-screen overflow-y-scroll">
+        <div className="flex flex-row w-fit ml-[30px]">
+          {
+            //Returns "No results found if no matching results are found
+            bookData.items
+              ? bookData.items.map((book) => {
+                  console.log(book);
+                  return (
+                    <div
+                      className="w-[400px] box-border m-[20px]"
+                      key={book.id}
+                    >
+                      <Link
+                        href={`/book/${book.id}`}
+                        className="flex flex-col"
+                      >
+                        <Image
                           //Returns another image saying "Image not found" if the api does not return any image links. Size has been
                           src={
                             book.volumeInfo.imageLinks
                               ? book.volumeInfo.imageLinks.thumbnail
                               : "https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg"
                           }
-                          className="w-[10vw]"
+                          width="100"
+                          height="100"
+                          className="w-[200px]"
                           alt={book.volumeInfo.title}
                         />
-                      </figure>
-                      <div className="card-body">
-                        <h2 className="card-title w-[20px]">
-                          {book.volumeInfo.title}
-                        </h2>
-                      </div>
-                    </Link>
-                  </div>
-                );
-              })
-            : "No results found"
-        }
+                        {/*TODO: make the text overflow hidden*/}
+                        <h2 className="">{book.volumeInfo.title}</h2>
+                      </Link>
+                    </div>
+                  );
+                })
+              : "No results found"
+          }
+        </div>
       </div>
     </div>
   ) : (
