bf58babc26bd16006cd1e256d255d396d20daa16
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-28 21:35:26 -0400

    Update home page to look like figma

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,23 @@
 import Navbar from "@/app/components/navbar";
 import Intro from "@/app/components/intro";
+import Divider from "@/app/components/divider";
 import BooksOfGenre from "@/app/booksOfGenre";
 
 export default function App() {
   return (
-    <div>
+    <div className="text-white">
       <Navbar></Navbar>
-      <Intro>
-
+      <Intro className="pt-[150px]">
+        <span className="text-[42px]  ">Welcome to the Book Diary</span>
+        <span className="text-[32px] w-[516px] text-center">
+          test test test test test test test test test test test test test test
+          test test test test test
+        </span>
       </Intro>
-      <BooksOfGenre genre="Fiction" />
-      <br />
-      <BooksOfGenre genre="Fantasy" />
+      <Divider className="mt-[20vh]"/>
+      <BooksOfGenre genre="Fiction" className="mt-[15vh]"/>
+      <Divider className="mt-[20vh]"/>
+      <BooksOfGenre genre="Fantasy" className="mt-[15vh]"/>
     </div>
   );
 }
