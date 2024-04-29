b3f4d4db31a195b6b17631222e5ccd9d3a9773ce
jashan <jashan20preet21@gmail.com>
2024-04-29 17:11:12 -0400

    Added margin to overall layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} mx-[20px] box-content`}>{children}</body>
     </html>
   );
 }
