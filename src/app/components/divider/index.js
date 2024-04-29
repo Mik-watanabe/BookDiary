c5233e64ae0cdda96e3e4d3bf105d6699bcdbfce
jashan <jashan20preet21@gmail.com>
2024-04-29 14:17:28 -0400

    Remove unnecessary div

--- src/app/components/divider/index.js
+++ src/app/components/divider/index.js
@@ -1,7 +1,5 @@
 export default function Divider(props ) {
   return (
-    <div className={`w-[100%] flex justify-center ${props.className}`}>
-      <div className="w-[90%] h-[2px] bg-white" />
-    </div>
+    <div className={`mx-[20px] h-[2px] bg-white box-content ${props.className}`}/>
   );
 }
