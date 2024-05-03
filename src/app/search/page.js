98483f6c5362f78823a5d35d0e308b17d1dd22f5
jashan <jashan20preet21@gmail.com>
2024-05-03 12:20:30 -0400

    add styling and navigation of books
    added the search bar and such.
    added a next button to go to next search results

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,9 +1,11 @@
 "use client";
 import axios from "axios";
 import Navbar from "@/app/components/navbar";
+import Intro from "@/app/components/intro";
+import Divider from "@/app/components/divider";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Search() {
   const searchParams = useSearchParams();
@@ -15,32 +17,45 @@ export default function Search() {
   useEffect(() => {
     const fetchData = async () => {
       let response = await axios.post("/getbooks", {
-        query: `intitle:${title}`,
+        query: `q=intitle:${title}&startIndex=${startIndex}&maxResults=10`,
       });
+      console.log(response);
       setBooks(response.data);
     };
     fetchData();
-  }, [title]);
+  }, [title, startIndex]);
 
   return (
-    <>
+    <div className="text-white">
       <Navbar />
-      {books
-        ? books.items
-          ? books.items.map((book) => {
-              return (
-                <Link href={`/book/${book.id}`} className="w-full flex">
-                  <h1 key={book.id}>
-                    {book.volumeInfo.title} -{" "}
-                    {book.volumeInfo.authors
-                      ? book.volumeInfo.authors[0]
-                      : "No author found"}
-                  </h1>
-                </Link>
-              );
-            })
-          : "No results found"
-        : "Fetching..."}
-    </>
+      <Intro className="my-[100px]">
+        <span className="text-[42px]  ">Find a Book here</span>
+      </Intro>
+      <Divider className="mb-[30px]"/>
+      <div>
+        {books
+          ? books.items
+            ? <>
+            <div className='flex justify-center mb-[30px]'>
+              Found {books.totalItems} matches for "{title}"
+            </div>
+            {books.items.map((book) => {
+                return (
+                  <Link href={`/book/${book.id}`} className="w-full flex">
+                    <h1 key={book.id}>
+                      {book.volumeInfo.title} -{" "}
+                      {book.volumeInfo.authors
+                        ? book.volumeInfo.authors[0]
+                        : "No author found"}
+                    </h1>
+                  </Link>
+                );
+              })}
+              </>
+            : "No results found"
+          : "Fetching..."}
+          <button className="bg-black" onClick={() => {setStartIndex(startIndex + 1)}}>Next</button>
+      </div>
+    </div>
   );
 }
