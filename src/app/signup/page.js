2b936714db4f07f1ebe05e21bd3238058f87b1cf
Jashanpreet2 <128256855+Jashanpreet2@users.noreply.github.com>
2024-05-26 14:43:05 -0400

    Complete basic form

---
+++ src/app/signup/page.js
@@ -0,0 +1,42 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    if (username.length == 0) {
+      alert("Username cannot be empty!");
+      return;
+    }
+    if (password.length < 8) {
+      alert("The password must be at least 8 characters!");
+      return;
+    }
+    axios({
+      method: "post",
+      url: "/user",
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+  };
+  return (
+    <div className="text-white">
+      <div className="h-[200px] flex flex-col justify-between items-center">
+        <h1 className="text-xl text-white">Sign up</h1>
+        <label htmlFor="username">Username</label>
+        <input name="username" onChange={(e) => setUsername(e.target.value)} className="text-black"/>
+        <label htmlFor="password">Password</label>
+        <input name="password" onChange={(e) => setPassword(e.target.value)} className="text-black"/>
+        <button
+          className="bg-blue-500 border-[2px] border-red-400"
+          onClick={handleSubmit}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+}
